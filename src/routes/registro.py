f67e9cbded19b985ac02ffebff3d3db06db5d0b4
Andres Felipe Rodriguez roa <89116331+japipe05@users.noreply.github.com>
2024-05-24 10:05:17 -0500

    correcion main

--- src/routes/registro.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, request
-from database.cassandra.conexion import session
-from models.login import verify_password
-from models.login import hash_password
-
-def registro():
-    #console.log("hello")
-    return render_template('registro.html')
-    
